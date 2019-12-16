a1267b1468113d28ed849640a95ff46e6ef767de
Roberto <rroberto.rruiz@gmail.com>
2019-12-15 23:39:05 -0600

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+const siteConfig = require('./site-config');
+
+module.exports = {
+  siteMetadata: {
+    ...siteConfig
+  },
+  plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1024
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants'
+        ]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: siteConfig.googleAnalyticsTrackingId
+      }
+    },
+
+    /* 
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/gatsby-icon.png`
+      }
+    }, */
+    //`gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`
+  ]
+};
---
+++ site-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  //
+  // Change information to your own:
+  //
+  // Site Info:
+  title: 'Customize your controller',
+  description: 'Would you like to try different color schemes for your game controllers? This is the site for you!',
+  siteUrl: 'https://colortroller.netlify.com/',
+  footerText:
+    'Try different colors!',
+
+  // Blog Author Info:
+  author: 'Roberto Ruiz',
+  authorImage:
+    'https://pbs.twimg.com/profile_images/986603715732934656/7A4Nt68k_400x400.jpg',
+  authorBio:
+    "Hello! My name is Roberto Ruiz.<br/><br/>I made this site as a side-project so people can try different color schemes for their controllers.<br/><br/>Have any suggestions? <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://twitter.com/robruizr\">Contact me on Twitter @RobRuizR!</a>",
+  social: {
+    twitter: 'robruizr',
+    github: 'robruizr'
+  },
+
+  // Services:
+  googleAnalyticsTrackingId: `ADD YOUR TRACKING ID HERE`,
+
+  // Site Style:
+  lightTheme: 'grommet', // grommet | dark | base | dxc | aruba
+  // leave darkTheme blank if you don’t need a theme switch
+  darkTheme: 'dark', // '' | grommet | dark | base | dxc | aruba
+  font: '', // Roboto
+  fontHeadings: '', // Merriweather
+  brandColor: '' // #7D4CDB
+};
